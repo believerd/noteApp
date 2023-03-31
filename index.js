7a8a30996532b47d233d5fd8d6f9be189c4e4d4d
believerd <30063871+believerd@users.noreply.github.com>
2023-03-31 20:19:46 +0800

    initialization

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(morgan('common'))
+app.use(express.static('build'))
+
+let notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        important: true
+    },
+    {
+        id: 2,
+        content: "Browser can execute only JavaScript",
+        important: false
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        important: true
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+})
+
+const generateId = () => {
+    const id = notes.length > 0
+        ? Math.max(...notes.map(note => note.id))
+        : 0
+    return id + 1
+}
+
+app.post('/api/notes', (req, res) => {
+    const body = req.body
+    if (!body.content) {
+        res.status(400).json({
+            error: 'content missing'
+        }).end()
+        return
+    }
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generateId()
+    }
+    notes = notes.concat(note)
+    res.json(note)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const note = notes.find(note => note.id === id)
+    if (note) {
+        res.json(note)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    const id = Number(req.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    res.status(204).end()
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
