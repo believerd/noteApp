323a5fb32bdc59a5d2800c0ddd91c62144a247a5
believerd <30063871+believerd@users.noreply.github.com>
2023-04-01 16:12:39 +0800

    updated routes

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const cors = require('cors')
 const Note = require('./models/note')
 
 const app = express()
+app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
 app.use(morgan('common'))
-app.use(express.static('build'))
 
 
 app.get('/api/notes', (req, res) => {
@@ -17,46 +17,68 @@ app.get('/api/notes', (req, res) => {
   })
 })
 
-const generateId = () => {
-  const id = notes.length > 0
-    ? Math.max(...notes.map(note => note.id))
-    : 0
-  return id + 1
-}
-
 app.post('/api/notes', (req, res) => {
   const body = req.body
-  if (!body.content) {
-    res.status(400).json({
-      error: 'content missing'
-    }).end()
-    return
+  if (body.content === undefined) {
+    return res.status(400).send({ error: 'content missing' })
   }
-  const note = {
+
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId()
+  })
+  note.save().then(savedNote => {
+    res.json(savedNote)
+  })
+})
+
+app.get('/api/notes/:id', (req, res, next) => {
+  Note.findById(req.params.id)
+    .then(note => {
+      if (note) {
+        res.json(note)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (req, res, next) => {
+  Note.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (req, res, next) => {
+  const body = req.body
+  const note = {
+    content: body.content,
+    important: body.important,
   }
-  notes = notes.concat(note)
-  res.json(note)
+
+  Note.findByIdAndUpdate(req.params.id, note, { new: true })
+    .then(updatedNote => {
+      res.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
-    res.json(note)
-  } else {
-    res.status(404).end()
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+  console.log(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
   }
-})
-
-app.delete('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  res.status(204).end()
-})
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
