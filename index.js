c0dafa02b6cf750cb633c7e60e0982a97bb84eb0
believerd <30063871+believerd@users.noreply.github.com>
2023-04-01 17:08:09 +0800

    severless way to connect DB

--- index.js
+++ index.js
@@ -3,12 +3,26 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const Note = require('./models/note')
+const mongoose = require('mongoose')
 
 const app = express()
 app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
 app.use(morgan('common'))
+
+const connectDB = async () => {
+  const url = process.env.MONGODB_URI
+  console.log('connecting to', url)
+  mongoose.set('strictQuery', false)
+  mongoose.connect(url)
+    .then(result => {
+      console.log('connected to MongoDB')
+    })
+    .catch(error => {
+      console.log('error connecting to MongoDB:', error.message)
+    })
+}
 
 
 app.get('/api/notes', (req, res) => {
@@ -81,6 +95,9 @@ const errorHandler = (error, req, res, next) => {
 app.use(errorHandler)
 
 const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
 })
