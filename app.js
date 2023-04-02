a572d4d842405693e48adcdd8845a6ed015fbc2a
believerd <30063871+believerd@users.noreply.github.com>
2023-04-02 20:52:27 +0800

    added usersRouter

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require('cors')
 const morgan = require('morgan')
 const notesRouter = require('./controllers/notes')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const database = require('./utils/database')
 
@@ -14,8 +15,11 @@ if (!database.isConnected()) {
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
-app.use(morgan('common'))
+if (!process.env.NODE_ENV === 'test') {
+  app.use(morgan('common'))
+}
 app.use('/api/notes', notesRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
