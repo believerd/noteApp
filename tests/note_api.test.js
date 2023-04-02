94e7105498264b6039b9f0d1ebffe701b054890b
believerd <30063871+believerd@users.noreply.github.com>
2023-04-02 17:48:35 +0800

    added more tests

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,24 +1,46 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const Note = require('../models/note')
 const app = require('../app')
-
 const api = supertest(app)
 
-test('notes are returned as json', async () => {
-  await api
-    .get('/api/notes')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-}, 100000)
+const initialNotes = [
+  {
+    content: "HTML is easy",
+    important: false,
+  },
+  {
+    content: "Browser can execute only JavaScript",
+    important: true,
+  },
+]
 
-test('there are two notes', async () => {
-  const res = await api.get('/api/notes')
-  expect(res.body).toHaveLength(2)
-})
+beforeEach(async () => {
+  await Note.deleteMany({})
+  let noteObject = new Note(initialNotes[0])
+  await noteObject.save()
+  noteObject = new Note(initialNotes[1])
+  await noteObject.save()
+}, 10000)
 
 test('the first note is about HTTP', async () => {
   const res = await api.get('/api/notes')
-  expect(res.body[0].content).toBe('HTML is hard!')
+  expect(res.body[0].content).toBe('HTML is easy')
+})
+
+test('all notes are returned', async () => {
+  const response = await api.get('/api/notes')
+
+  expect(response.body).toHaveLength(initialNotes.length)
+})
+
+test('a specific note is within the returned notes', async () => {
+  const response = await api.get('/api/notes')
+
+  const contents = response.body.map(r => r.content)
+  expect(contents).toContainEqual(
+    'Browser can execute only JavaScript'
+  )
 })
 
 afterAll(async () => {
