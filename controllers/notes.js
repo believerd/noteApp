b311acf7d7555d6bf9686856e72f7f8e8f791c42
believerd <30063871+believerd@users.noreply.github.com>
2023-04-03 10:26:32 +0800

    added basic users

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,21 +1,26 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 
 notesRouter.get('/', async (req, res) => {
-  const notes = await Note.find({})
+  const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
   res.send(notes)
 })
 
 notesRouter.post('/', async (req, res) => {
   const body = req.body
+  const user = await User.findById(body.userId)
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user.id
   })
 
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
   res.status(201).send(savedNote)
 })
 
