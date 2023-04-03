e78221929952629e49304adb2d5c421ddbe0d1d7
believerd <30063871+believerd@users.noreply.github.com>
2023-04-03 17:16:28 +0800

    added login to frontend

--- utils/middleware.js
+++ utils/middleware.js
@@ -5,8 +5,7 @@ const unknownEndpoint = (req, res) => {
 
 
 const errorHandler = (error, req, res, next) => {
-  console.log(error.name)
-  console.log(error.message)
+  console.log(error.name, ' : ', error.message)
 
   if (error.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id' })
