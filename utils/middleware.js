8c9b8879b1b5735063d4135fd4b0a1c1223c3851
believerd <30063871+believerd@users.noreply.github.com>
2023-04-03 14:47:42 +0800

    added token based authorization

--- utils/middleware.js
+++ utils/middleware.js
@@ -13,6 +13,10 @@ const errorHandler = (error, req, res, next) => {
   }
   else if (error.name === 'ValidationError') {
     return res.status(400).send({ error: error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    return res.status(400).send({ error: error.message })
+  } else if (error.name === 'TokenExpiredError') {
+    return res.status(401).send({ errro: 'token expired' })
   }
 
   next(error)
